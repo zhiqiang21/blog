c94447eca624bdfea849fa45c913babca4807d4a
ZhiQiangHou <hpuhouzhiqiang@gmail.com>
2016-07-18 22:03:50 +0800

    edit blog

---
+++ Demo/JavaScript面向对象（一）/test.js
@@ -0,0 +1,30 @@
+/**
+ * Created by baidu on 16/7/17.
+ */
+
+
+//string
+
+var name='zhiqiang';
+var selection='a';
+
+//number
+
+var count=235;
+var cost=1.51;
+
+//boolean
+
+var found=true;
+
+//null
+
+var object=null;
+
+//undefined
+
+var flag=undefined;
+var ref;
+
+console.log(ref);  //undefined
+
